7215180ab4e8f28d49eae339c0b1e1eb5ee32790
Jared Heinrich <jared.heinrich04@gmail.com>
2026-04-23 20:59:53 +0200

    Implement archive command

--- src/file_operations.rs
+++ src/file_operations.rs
@@ -2,26 +2,27 @@ use anyhow::Error;
 use anyhow::Result;
 use std::{
     fs::{self, File},
-    path::PathBuf,
+    path::Path,
     process::Command,
 };
 
 use crate::error::FileSystemError;
 
 pub trait FileOperations {
-    fn get_files(&self, dir: &PathBuf) -> Result<Vec<String>>;
-    fn delete_file(&mut self, path: &PathBuf) -> Result<()>;
-    fn create_file(&mut self, path: &PathBuf) -> Result<()>;
-    fn create_dir(&mut self, path: &PathBuf) -> Result<()>;
-    fn open_file(&mut self, editor_command: &str, path: &PathBuf) -> Result<()>;
-    fn exists(&self, path: &PathBuf) -> Result<bool>;
-    fn read_file(&self, path: &PathBuf) -> Result<String>;
-    fn write_file(&self, path: &PathBuf, value: &str) -> Result<()>;
+    fn get_files(&self, dir: &Path) -> Result<Vec<String>>;
+    fn delete_file(&mut self, path: &Path) -> Result<()>;
+    fn create_file(&mut self, path: &Path) -> Result<()>;
+    fn create_dir(&mut self, path: &Path) -> Result<()>;
+    fn open_file(&mut self, editor_command: &str, path: &Path) -> Result<()>;
+    fn exists(&self, path: &Path) -> Result<bool>;
+    fn read_file(&self, path: &Path) -> Result<String>;
+    fn write_file(&mut self, path: &Path, value: &str) -> Result<()>;
+    fn copy(&mut self, source_path: &Path, destination_path: &Path) -> Result<()>;
 }
 
 pub struct FileSystem;
 impl FileOperations for FileSystem {
-    fn get_files(&self, dir: &PathBuf) -> Result<Vec<String>> {
+    fn get_files(&self, dir: &Path) -> Result<Vec<String>> {
         let files: Vec<String> = fs::read_dir(dir)?
             .map(|file| file.unwrap().file_name().into_string().unwrap())
             .filter(|file| !file.starts_with("."))
@@ -29,21 +30,21 @@ impl FileOperations for FileSystem {
         Ok(files)
     }
 
-    fn delete_file(&mut self, path: &PathBuf) -> Result<()> {
+    fn delete_file(&mut self, path: &Path) -> Result<()> {
         fs::remove_file(path).map_err(Error::from)
     }
 
-    fn create_file(&mut self, path: &PathBuf) -> Result<()> {
+    fn create_file(&mut self, path: &Path) -> Result<()> {
         File::create_new(path).map_err(Error::from).map(|_| ())
     }
 
-    fn create_dir(&mut self, path: &PathBuf) -> Result<()> {
+    fn create_dir(&mut self, path: &Path) -> Result<()> {
         fs::create_dir_all(path).map_err(Error::from)
     }
 
-    fn open_file(&mut self, editor_command: &str, path: &PathBuf) -> Result<()> {
+    fn open_file(&mut self, editor_command: &str, path: &Path) -> Result<()> {
         if !path.is_file() {
-            return Err(FileSystemError::NotAFile(path.clone()).into());
+            return Err(FileSystemError::NotAFile(path.to_path_buf()).into());
         }
         Command::new(editor_command)
             .arg(path.as_os_str())
@@ -52,18 +53,23 @@ impl FileOperations for FileSystem {
             .map_err(|e| e.into())
     }
 
-    fn exists(&self, path: &PathBuf) -> Result<bool> {
+    fn exists(&self, path: &Path) -> Result<bool> {
         fs::exists(path).map_err(Into::into)
     }
 
-    fn read_file(&self, path: &PathBuf) -> Result<String> {
+    fn read_file(&self, path: &Path) -> Result<String> {
         fs::read_to_string(path).map_err(Into::into)
     }
 
-    fn write_file(&self, file_path: &PathBuf, content: &str) -> Result<()> {
-        let mut dir_path = file_path.clone();
+    // TODO remove create_dir_all
+    fn write_file(&mut self, file_path: &Path, content: &str) -> Result<()> {
+        let mut dir_path = file_path.to_path_buf();
         dir_path.pop();
         fs::create_dir_all(dir_path)?;
         fs::write(file_path, content).map_err(Into::into)
+    }
+
+    fn copy(&mut self, source_path: &Path, destination_path: &Path) -> Result<()> {
+        fs::copy(source_path, destination_path).map(|_|()).map_err(Into::into)
     }
 }
